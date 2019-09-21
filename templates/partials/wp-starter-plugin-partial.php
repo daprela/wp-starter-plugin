b79f208a898087e0c37c9530a1784a13f34ab716
Giulio <giulio.daprela@gmail.com>
2019-09-21 11:42:39 +0700

    Removed redundant comments from templates

--- templates/partials/wp-starter-plugin-partial.php
+++ templates/partials/wp-starter-plugin-partial.php
@@ -1,18 +1,2 @@
-<?php
-
-namespace dapre_wpsp\templates;
-
-/**
- * Provide a admin area view for the plugin
- *
- * This file is used to markup the admin-facing aspects of the plugin.
- *
- * @link       https://giuliodaprela.com
- * @since      1.0.0
- *
- * @package    dapre_wpsp
- * @subpackage dapre_wpsp/templates
- */
-?>
 
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
--- templates/wp-starter-plugin-template.php
+++ templates/wp-starter-plugin-template.php
@@ -1,19 +1,2 @@
-<?php
-
-namespace test_namespace\templates;
-
-/**
- * Provide a admin area view for the plugin
- *
- * This file is used to markup the admin-facing aspects of the plugin.
- *
- * @author     Panos <giulio.daprela@gmail.com>
- * @link       https://giuliodaprela.com
- * @since      1.0.0
- *
- * @package    test_namespace
- * @subpackage test_namespace/templates
- */
-?>
 
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
