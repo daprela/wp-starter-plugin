d64fff744964ed01f5552e42d0c6d8d8d29c9ad4
Giulio <giulio.daprela@gmail.com>
2021-04-16 21:02:10 +0700

    Working on it

--- webpackNoGutenberg.dev.js
+++ webpackNoGutenberg.dev.js
@@ -1,5 +1,8 @@
-/* eslint-disable import/no-duplicates,new-cap */
 const path = require('path');
+// eslint-disable-next-line import/no-unresolved
+const ForkTsCheckerNotifierWebpackPlugin = require('fork-ts-checker-notifier-webpack-plugin');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -11,32 +14,53 @@ module.exports = {
     path: path.resolve(__dirname),
     filename: '[name].js',
   },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
   devtool: 'source-map',
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({
+      eslint: true
+    }),
+    new ForkTsCheckerNotifierWebpackPlugin({
+      title: 'TypeScript',
+      excludeWarnings: false
+    }),
+  ],
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(tsx|ts|js)$/,
         exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              '@wordpress/default',
-              ['@babel/preset-env', {
-                targets: {
-                  // The % refers to the global coverage of users from browserslist
-                  browsers: ['>2%', 'not ie 11', 'not op_mini all'],
-                },
-              }],
-            ],
-            plugins: [
-              [
-                '@babel/transform-react-jsx',
-                { pragma: 'wp.element.createElement' },
-              ],
-            ],
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              // disable type checker - we will use it in fork plugin
+              transpileOnly: true
+            }
           },
-        },
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@wordpress/default',
+                ['@babel/preset-env', {
+                  targets: {
+                    // The % refers to the global coverage of users from browserslist
+                    browsers: ['>2%', 'not ie 11', 'not op_mini all'],
+                  },
+                }],
+              ],
+              plugins: [
+                [
+                  '@babel/transform-react-jsx',
+                  { pragma: 'wp.element.createElement' },
+                ],
+              ],
+            },
+          }
+        ],
       },
     ],
   },
